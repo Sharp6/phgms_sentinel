635f316308fa12581de627849ba86c95ea43482e
Philip Huysmans <philip.huysmans@gmail.com>
2016-05-28 00:13:19 +0200

    started adding db functionality

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 module.exports = function(dependencies, gms) {
 	function doCheck() {
 		var state = gms.getState();
+		var registeredChangedTimestamp = dependencies.moment("2000-01-01");
+
 		dependencies.logger.info("SENTINEL: state:", state.state, "updated:", state.stateLastUpdatedTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a"), "changed:", state.stateLastChangedTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-		console.log("SENTINEL: doing a check.", dependencies.moment().diff(state.stateLastChangedTimestamp, 'minutes'));
+		
+		if(registeredChangedTimestamp.diff(dependencies.moment(state.stateLastChangedTimestamp)) < 0) {
+			dependencies.db.insert(state);
+			registeredChangedTimestamp = state.stateLastChangedTimestamp;
+		}
+
+
 		if(state.state == "ON" && dependencies.moment().diff(state.stateLastChangedTimestamp, 'minutes') > 1) {
 			console.log("SENTINEL: ALARM");
 		}
